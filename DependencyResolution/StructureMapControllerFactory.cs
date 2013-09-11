01627e8a713151955167ec9bb8878b08915a946d
colonaut <colonaut@gmx.de>
2013-09-11 21:47:52 +0200

    initial push to GitHub

---
+++ DependencyResolution/StructureMapControllerFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+using StructureMap;
+
+namespace ControllerFactory.DependencyResolution
+{
+    public class StructureMapControllerFactory : DefaultControllerFactory
+    {
+        protected override IController GetControllerInstance(RequestContext context, Type type)
+        {
+            return ObjectFactory.GetInstance(type) as Controller;
+        }
+    }    
+}
